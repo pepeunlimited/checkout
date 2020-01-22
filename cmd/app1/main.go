e5158579287a4607ed5e59825f7e41584b3ca43e
Simo Ala-Kotila <simo.alakotila@gmail.com>
2020-01-22 19:22:04 +0200

    fixed main.go

--- cmd/app1/main.go
+++ cmd/app1/main.go
@@ -1,25 +1,29 @@
 package main
 
 import (
+	"github.com/pepeunlimited/accounts/accountsrpc"
 	"github.com/pepeunlimited/checkout/internal/app/app1/server"
 	"github.com/pepeunlimited/checkout/checkoutrpc"
-	"github.com/pepeunlimited/microservice-kit/headers"
 	"github.com/pepeunlimited/microservice-kit/middleware"
+	"github.com/pepeunlimited/microservice-kit/misc"
 	"log"
 	"net/http"
 )
 
 const (
-	Version = "0.1"
+	Version = "0.1.1"
 )
 
 func main() {
 	log.Printf("Starting the CheckoutServer... version=[%v]", Version)
 
-	ts := checkoutrpc.NewTodoServiceServer(server.NewCheckoutServer(), nil)
+	accounts := accountsrpc.NewAccountServiceProtobufClient(misc.GetEnv(accountsrpc.RpcAccountsHost, "api.dev.pepeunlimited.com"), http.DefaultClient)
+
+
+	cs := checkoutrpc.NewCheckoutServiceServer(server.NewCheckoutServer(accounts), nil)
 
 	mux := http.NewServeMux()
-	mux.Handle(ts.PathPrefix(), middleware.Adapt(ts, headers.Username()))
+	mux.Handle(cs.PathPrefix(), middleware.Adapt(cs))
 
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Panic(err)
